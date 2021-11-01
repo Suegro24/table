2a2ea813123831f4f465b36aa7161e770c4bf3c0
Suegro24 <marciu199@gmail.com>
2021-11-01 16:27:16 +0100

    add median, fix sort

--- main.js
+++ main.js
@@ -2,18 +2,6 @@ const table = {
     data: [],
     currentData: [],
     loading: true,
-    medians: [{
-            name: 'Profit TM',
-            median: 0
-        },
-        {
-            name: 'Profit WAX',
-            median: 0
-        },
-        {
-            name: 'Profit Shadow',
-            median: 0
-        }],
     headers: [{
         name: 'Item',
         sort: 'unsorted',
@@ -63,11 +51,15 @@ const table = {
         name: 'Profit Shadow',
         sort: 'unsorted',
         index: 9
+    },
+    {
+        name: 'Median',
+        sort: 'unsorted',
+        index: 10
     }],
     run: async function() {
         await this.loadData().then(() => {
             this.createTable();
-            this.createMedianContainer();
             this.toggleLoading(false);
         })
         this.initSearch();
@@ -82,17 +74,6 @@ const table = {
         })
         this.data = json.items;
         this.currentData = json.items;
-        let medianValuesProfitTm = [];
-        let medianValuesProfitWax = [];
-        let medianValuesProfitShadow = [];
-        this.currentData.map(function(value) {
-            medianValuesProfitTm.push(table.getItemValueByHeaderIndex(value, 10));
-            medianValuesProfitWax.push(table.getItemValueByHeaderIndex(value, 11));
-            medianValuesProfitShadow.push(table.getItemValueByHeaderIndex(value, 12));
-        })
-        table.medians[0].median = Math.round(table.getMedian(medianValuesProfitTm)*100) + '%';
-        table.medians[1].median = Math.round(table.getMedian(medianValuesProfitWax)*100) + '%';
-        table.medians[2].median = Math.round(table.getMedian(medianValuesProfitShadow)*100) + '%';
     },
     toggleLoading: async function(state) {
         const loading = document.querySelector('#loading');
@@ -113,6 +94,13 @@ const table = {
         table += `</tr>`;
             for (let i = 0; i < this.currentData.length; i++) {
                 try {
+                    const profitTM = Math.round((this.currentData[i].prices.csgotm_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100);
+                    const profitWAX = Math.round((this.currentData[i].prices.waxpeer_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100);
+                    const profitShadow = Math.round((this.currentData[i].prices.shadowpay_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100);
+                    const maxProfit = profitTM > profitWAX ? profitTM > profitShadow ? 'TM' : 'Shadow' : profitWAX > profitShadow ? 'WAX' : 'Shadow';
+                    const minProfit = profitTM < profitWAX ? profitTM < profitShadow ? 'TM' : 'Shadow' : profitWAX < profitShadow ? 'WAX' : 'Shadow';
+                    
+                    const medianProfit = this.getMedian([profitTM, profitWAX, profitShadow]);
                     table += `
                     <tr>
                         <td>${this.currentData[i].name}</td>
@@ -122,46 +110,19 @@ const table = {
                         <td>${this.currentData[i].prices.csgotm_avg7.price != 0 ? this.currentData[i].prices.csgotm_avg7.price/100 : '-'}</td>
                         <td>${this.currentData[i].prices.waxpeer_avg7.price != 0 ? this.currentData[i].prices.waxpeer_avg7.price/100 : '-'}</td>
                         <td>${this.currentData[i].prices.shadowpay_avg7.price != 0 ? this.currentData[i].prices.shadowpay_avg7.price/100 : '-'}</td>
-                        <td>${(this.currentData[i].prices.csgotm_avg7.price - this.currentData[i].prices.buff163.price) != 0 ? (this.currentData[i].prices.csgotm_avg7.price - this.currentData[i].prices.buff163.price)/100 : '-'}</td>
-                        <td>${(this.currentData[i].prices.waxpeer_avg7.price - this.currentData[i].prices.buff163.price) != 0 ? (this.currentData[i].prices.waxpeer_avg7.price - this.currentData[i].prices.buff163.price)/100 : '-'}</td>
-                        <td>${(this.currentData[i].prices.shadowpay_avg7.price - this.currentData[i].prices.buff163.price) != 0 ? (this.currentData[i].prices.shadowpay_avg7.price - this.currentData[i].prices.buff163.price)/100 : '-'}</td>
-                        <td>${Math.round((this.currentData[i].prices.csgotm_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100) > -100 ? 
-                            Math.round((this.currentData[i].prices.csgotm_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100) + '%' : 
-                            '-'
-                        }</td>
-                        <td>${Math.round((this.currentData[i].prices.waxpeer_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100) > -100 ?
-                            Math.round((this.currentData[i].prices.waxpeer_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100) + '%' :
-                            '-'
-                        }</td>
-                        <td>${Math.round((this.currentData[i].prices.shadowpay_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100) > -100 ? 
-                            Math.round((this.currentData[i].prices.shadowpay_avg7.price - this.currentData[i].prices.buff163.price)/this.currentData[i].prices.buff163.price * 100) + '%' :
-                            '-'
-                        }</td>
+                        <td class="${maxProfit === 'TM' ? 'background-green' : minProfit === 'TM' ? 'background-red' : ''}">${ profitTM > -100 ? profitTM + '%' : '-'}(${(this.currentData[i].prices.csgotm_avg7.price - this.currentData[i].prices.buff163.price) != 0 ? (this.currentData[i].prices.csgotm_avg7.price - this.currentData[i].prices.buff163.price)/100 + '$' : '-'})</td>
+                        <td class="${maxProfit === 'WAX' ? 'background-green' : minProfit === 'WAX' ? 'background-red' : ''}">${ profitWAX > -100 ? profitWAX + '%' : '-'}(${(this.currentData[i].prices.waxpeer_avg7.price - this.currentData[i].prices.buff163.price) != 0 ? (this.currentData[i].prices.waxpeer_avg7.price - this.currentData[i].prices.buff163.price)/100 + '$' : '-'})</td>
+                        <td class="${maxProfit === 'Shadow' ? 'background-green' : minProfit === 'Shadow' ? 'background-red' : ''}">${ profitShadow > -100 ? profitShadow + '%' : '-'}(${(this.currentData[i].prices.shadowpay_avg7.price - this.currentData[i].prices.buff163.price) != 0 ? (this.currentData[i].prices.shadowpay_avg7.price - this.currentData[i].prices.buff163.price)/100 + '$' : '-'})</td>
+                        <td>${medianProfit}%</td>
                     </tr>
                 `
                 } catch(error) {
+                    console.log(error);
                     continue;
                 }
             }
             const main = document.querySelector('#main');
             main.querySelector('.table').innerHTML = table;
-    },
-    createMedianContainer: function() {
-        const medianContainer = document.querySelector('.medianContainer');
-        let min = table.medians[0];
-        let max = table.medians[0];
-        table.medians.map(median => {
-            if (median.median < min.median) min = median;
-            if (median.median > max.median) max = median;
-        })
-        let medianList = `
-            <ul>
-                <li class="${min.name === table.medians[0].name ? 'background-red' : max.name === table.medians[0].name ? 'background-green' : ''}">${table.medians[0].name}: ${table.medians[0].median}</li>
-                <li class="${min.name === table.medians[1].name ? 'background-red' : max.name === table.medians[1].name ? 'background-green' : ''}">${table.medians[1].name}: ${table.medians[1].median}</li>
-                <li class="${min.name === table.medians[2].name ? 'background-red' : max.name === table.medians[2].name ? 'background-green' : ''}">${table.medians[2].name}: ${table.medians[2].median}</li>
-            </ul>
-        `
-        medianContainer.innerHTML += medianList;
     },
     initSearch: function() {
         const search = document.querySelector('#search');
@@ -244,7 +205,7 @@ const table = {
             items[rightIndex] = temp;
         }
         table.toggleLoading(true);
-        table.currentData = table.currentData.filter(item => table.getItemValueByHeaderIndex(item, headerIndex));
+        table.currentData = table.currentData.filter(item => table.getItemValueByHeaderIndex(item, headerIndex) > -1 && isFinite(table.getItemValueByHeaderIndex(item, headerIndex)));
         await quickSort(table.currentData, 0, table.currentData.length - 1, headerIndex, sortType).then(result => {
             table.currentData = result;
             table.createTable();
@@ -285,9 +246,15 @@ const table = {
                 case 9: {
                     return ((item.prices.shadowpay_avg7.price - item.prices.buff163.price)/item.prices.buff163.price)
                 }
+                case 10: {
+                    const profitTM = Math.round((item.prices.csgotm_avg7.price - item.prices.buff163.price)/item.prices.buff163.price * 100);
+                    const profitWAX = Math.round((item.prices.waxpeer_avg7.price - item.prices.buff163.price)/item.prices.buff163.price * 100);
+                    const profitShadow = Math.round((item.prices.shadowpay_avg7.price - item.prices.buff163.price)/item.prices.buff163.price * 100);
+                    return table.getMedian([profitTM, profitWAX, profitShadow]);
+                }
             }
         } catch(error) {
-            return '-';
+            return undefined;
         }
         
     },
@@ -306,18 +273,30 @@ const table = {
         return (values[half - 1] + values[half]) / 2.0;
     },
     initFilterForm: function() {
-        const form = document.querySelector('#filterForm');
-        form.addEventListener('click', function(event) {
-            event.preventDefault();
-            const from = form.elements.namedItem('from').value;
-            const to = form.elements.namedItem('to').value;
-            if (!from || !to) return;
-            table.currentData = table.currentData.filter(item => {
-                return table.getItemValueByHeaderIndex(item, 3)/100 >= from && table.getItemValueByHeaderIndex(item, 3)/100 <= to
-            })
-            table.createTable();
-            table.initSort();
-        });
+        const forms = document.querySelectorAll('.filterForm');
+        forms.forEach((form, index) => {
+            form.addEventListener('click', function(event) {
+                event.preventDefault();
+                const from = form.elements.namedItem('from').value;
+                const to = form.elements.namedItem('to').value;
+                if (!from || !to) return;
+                if (index == 0) {
+                    table.currentData = table.data.filter(item => {
+                        return table.getItemValueByHeaderIndex(item, 3)/100 >= from && table.getItemValueByHeaderIndex(item, 3)/100 <= to
+                    })
+                } else if (index == 1) {
+                    table.currentData = table.data.filter(item => {
+                        return table.getItemValueByHeaderIndex(item, 1) >= from && table.getItemValueByHeaderIndex(item, 1) <= to
+                    })
+                } else {
+                    table.currentData = table.data.filter(item => {
+                        return table.getItemValueByHeaderIndex(item, 10) >= from && table.getItemValueByHeaderIndex(item, 10) <= to
+                    })
+                }
+                table.createTable();
+                table.initSort();
+            });
+        })
     },
 }
 
